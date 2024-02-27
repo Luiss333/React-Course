d8d44edb299d307bbc5d50db6a0ce70eb9af20ea
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-02-27 11:42:32 -0600

    Creacion de objeto literal

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,15 +1,7 @@
 
 
-const nombre = 'Luis';
-const apellido = 'Martinez';
+const persona = {
 
-// const nombreCompleto = nombre + ' ' + apellido;
-const nombreCompleto = `${nombre} ${apellido}`; //Templates
+};
 
-console.log(nombreCompleto); 
-
-function getSaludo(nombre) {
-    return 'Hola ' + nombre;
-}
-
-console.log(`Este es un texto: ${getSaludo(nombre)}`); 
+console.log(persona);
