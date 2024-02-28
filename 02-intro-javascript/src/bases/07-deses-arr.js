23f649af2c3b2ebe18db7c8e45536457d678844a
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-02-28 10:08:42 -0600

    Notas adicionales en 07-deses-arr

--- 02-intro-javascript/src/bases/07-deses-arr.js
+++ 02-intro-javascript/src/bases/07-deses-arr.js
@@ -6,16 +6,28 @@ const [ , ,p3 ] = personajes;
 
 console.log(p3);
 
-const retornaArreglo = () => {
+const retornaArreglo = () => { //Se crea una función que retorna un arreglo
     return ['ABC', 123];
 }
 
-const [letras, numeros] = retornaArreglo();
+const [letras, numeros] = retornaArreglo(); //Se desestructura el arreglo que retorna la función
+
 console.log(letras, numeros);
 
 // Tarea
 // 1. el primer valor del arr se llamará nombre
 // 2. se llamará setNombre
+
+/*const usState = (valor) => { //Se crea una función que retorna un arreglo
+    return [valor, ()=>{console.log('Hola Mundo')}]; //Se retorna un arreglo con un valor en primera posicion y una función en la segunda
+};
+
+const arr = usState('Goku'); //Se guarda el arreglo que retorna la función en una variable
+console.log(arr); //Se imprime el arreglo
+console.log(arr[0]); //Se imprime el primer valor del arreglo
+arr[1](); //Se ejecuta la función que se encuentra en la segunda posición del arreglo
+
+*/
 
 const usState = (valor) => {
     return [valor, ()=>{console.log('Hola Mundo')}];
