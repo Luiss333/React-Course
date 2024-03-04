e3b4031f2337b42bc2cdce4f68a50d95f1b1005a
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-03-04 16:27:05 -0600

    Fragment - <></>

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,5 +1,10 @@
-function FirstApp (){
-    return <h1>Luis Gerardo Martinez Garcia</h1>
+function FirstApp() {
+  return (
+    <>
+      <h1>Luis Gerardo Martinez Garcia</h1>
+      <p>Soy un subtitulo</p>
+    </>
+  );
 }
 
 export default FirstApp;
