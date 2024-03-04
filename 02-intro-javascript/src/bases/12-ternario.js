a87d5266b25681ad7d0e219481478d5582d62494
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-03-04 13:02:22 -0600

    12-ternario agregado

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,15 @@
+
+
+const activo = true;
+
+// let mensaje = '';
+
+// if (!activo) {
+//     mensaje = 'Activo';
+// } else {
+//     mensaje = 'Inactivo';
+// }
+//const mensaje = (activo) ? 'Activo' : 'Inactivo'; // Operador ternario
+const mensaje = activo && 'Activo'; 
+
+console.log(mensaje);
