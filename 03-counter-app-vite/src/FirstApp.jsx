ceee7f6c00ac28fbfe407fb1de0b7825ef9608c8
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-03-06 10:43:39 -0600

    impresion de variables

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,20 @@
-function FirstApp() {
+/* const newMessage = {
+  message: "Hola Mundo",
+  title: "Luis",
+}; */
+
+/* const getResult = (a, b) => {
+  return a + b;
+};
+ */
+export const FirstApp = () => {
   return (
     <>
-      <h1>Luis Gerardo Martinez Garcia</h1>
+      {/* <h1>Hola Mundo</h1> */}
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
+      {/* <h1>{getResult(1, 5)}</h1> */}
+      <h1>Luis</h1>
       <p>Soy un subtitulo</p>
     </>
   );
-}
-
-export default FirstApp;
+};
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,11 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-import { HelloWorldApp } from './HelloWorldApp';
-import FirstApp from './FirstApp';
+import { HelloWorldApp } from "./HelloWorldApp";
+import { FirstApp } from "./FirstApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <FirstApp />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FirstApp />
+  </React.StrictMode>
 );
