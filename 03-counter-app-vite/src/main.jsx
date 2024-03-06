a4ccaf700d8cbe6f9f2213f521a6c43e44e7a239
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-03-06 13:21:21 -0600

    Tarea: Creacion de componente CounterApp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
+import { CounterApp } from "./CounterApp";
 
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp /* title="Hola, soy Luis" */ /* subTitle={123} */ />
+    {/* <FirstApp  title="Hola, soy Luis" subTitle={123}  />*/}
+    <CounterApp value={10} />
   </React.StrictMode>
 );
