02c8fd9d2bf1cdfbb1b11e162710a624fcfd5391
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-03-06 11:16:06 -0600

    props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -7,14 +7,16 @@
   return a + b;
 };
  */
-export const FirstApp = () => {
+export const FirstApp = (/* props */ { title, subTitle }) => {
+  // console.log(props);
   return (
     <>
       {/* <h1>Hola Mundo</h1> */}
       {/* <code>{JSON.stringify(newMessage)}</code> */}
       {/* <h1>{getResult(1, 5)}</h1> */}
-      <h1>Luis</h1>
-      <p>Soy un subtitulo</p>
+      {/* <h1>{props.title}</h1> */}
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
     </>
   );
 };
