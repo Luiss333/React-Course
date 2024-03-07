a3c695696f78e28d402924387fb29805344d688b
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-03-07 12:47:04 -0600

    Tarea: pruieba con toEqual

--- 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
+++ 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -1,4 +1,5 @@
 import { getUser } from '../../src/base-pruebas/05-funciones';
+import { getUsuarioActivo } from '../../src/base-pruebas/05-funciones';
 
 describe('Pruebas en 05-funciones', () => {
     test('getUser debe de retornar un objeto', () => {
@@ -10,5 +11,15 @@ describe('Pruebas en 05-funciones', () => {
         const user = getUser();
         console.log(user);
         expect(testUser).toEqual(user);
+    });
+
+    //Tarea
+    test('getUsuarioActivo debe de retornar un objeto', () => {
+        const name = 'Luis';
+        const testUsuarioActivo = getUsuarioActivo(name);
+        expect(testUsuarioActivo).toEqual({
+            uid: 'ABC567',
+            username: name
+        });
     });
 });
