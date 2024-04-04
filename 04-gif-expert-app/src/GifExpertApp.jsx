0fc50f6e69258b8ff6615d262e76c5dd3c449f47
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-03 19:50:32 -0600

    Estableciendo estructura y añadiendo estilos

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,13 @@
 export const GifExpertApp = () => {
   return (
     <>
+      {/* titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gifs */}
+      {/* Gif Item */}
     </>
   );
 };
