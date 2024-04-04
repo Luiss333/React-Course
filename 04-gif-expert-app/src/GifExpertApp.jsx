8ca0b760e781c8968210c96bfabe9edf607d7e8c
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-04 16:29:28 -0600

    Validando que los nombres (keys) sean unicos

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -5,6 +5,7 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
   const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     setCategories([newCategory, ...categories]); //...categories es para que no se pierda la información que ya tenía
   };
 
