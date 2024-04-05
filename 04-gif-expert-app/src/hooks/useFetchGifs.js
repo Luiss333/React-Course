da2853ddd01a3e6ee9658d01be1912d5b41291b1
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-05 04:40:00 -0600

    Primer Custom Hook creado y agregado useFetchGifs

---
+++ 04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  return {
+    images, // images: images (key y value son iguales)
+    isLoading, // isLoading: isLoading (key y value son iguales)
+  };
+};
