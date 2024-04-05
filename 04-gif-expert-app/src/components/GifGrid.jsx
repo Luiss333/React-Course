695560d15d01fdf921695a83f27f0b07587bdfac
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-05 03:18:09 -0600

    useEffect implementado

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,7 +1,12 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
   return (
     <>
