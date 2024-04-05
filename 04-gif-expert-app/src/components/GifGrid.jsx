bf6b21a5ef3ea14b914730371d394a4dc927e8c1
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-05 03:40:16 -0600

    Mostrando el listado de los titulos de los gifs obtenidos

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,14 +1,27 @@
 import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { useState } from "react";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
   useEffect(() => {
-    getGifs(category);
+    getImages();
   }, []);
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
