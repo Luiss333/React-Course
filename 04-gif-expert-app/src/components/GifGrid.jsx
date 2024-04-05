c510143550890e8d6e206b7ebee5fda0d49d32b6
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-05 03:26:43 -0600

    Creacion de produccion

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  const [counter, setCounter] = useState(10);
-
   useEffect(() => {
     getGifs(category);
   }, []);
