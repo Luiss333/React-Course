5b934237dac53c0bbccbceb21f4faa8833298ff6
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-05 03:03:08 -0600

    getGifs agregado y Fetch API configurado

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,12 +1,11 @@
+import { getGifs } from "../helpers/getGifs";
+
 export const GifGrid = ({ category }) => {
-  const gifs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  getGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
-      {gifs.map((gif) => (
-        <p>{gif}</p>
-      ))}
     </>
   );
 };
