06e4fbf5187f6b3702cf3cfc29aac20ff6a508d6
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-07 04:01:53 -0600

    Haciendo un Mock completo del customHook useFetchGifs y realizando pruebas

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,15 +1,45 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs");
 
 describe("Pruebas en <GifGrid/>", () => {
   const category = "One Piece";
 
   test("Debe de mostrar el loadinf inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
     render(<GifGrid category={category} />);
     expect(screen.getByText("Cargando..."));
     expect(screen.getByText(category));
     //screen.debug();
   });
 
-  test("Debe de mostrar items cuando se cargan las imagenes useFetchGifs", () => {});
+  test("Debe de mostrar items cuando se cargan las imagenes useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Luffy",
+        url: "https://localhost/luffy.jpg",
+      },
+      {
+        id: "123",
+        title: "Zoro",
+        url: "https://localhost/zoro.jpg",
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole("img").length).toBe(2);
+    //screen.debug();
+  });
 });
