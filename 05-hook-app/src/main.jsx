356a5e431a9fa49ce4dd36a6fe137ecf6c41db25
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-10 16:04:55 -0600

    Creacion de un customHook: useCounter

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HooksApp } from "./HooksApp";
 import { CounterApp } from "./01-useState/CounterApp";
+import { CounerWithCustomHook } from "./01-useState/CounerWithCustomHook";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounerWithCustomHook />
   </React.StrictMode>
 );
