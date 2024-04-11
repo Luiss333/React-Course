0525deba50fd42d5d7d0ab14935094a40d780fbe
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-11 03:58:49 -0600

    Parametrizando y consumiendo el customHook:useFetch

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { useFetch } from "../hooks";
 
 export const MultipleCustomHooks = () => {
-  useFetch();
-  return <div>MultipleCustomHooks</div>;
+  const { data, isLoading, hasError } = useFetch(
+    "https://pokeapi.co/api/v2/pokemon/3"
+  );
+  return (
+    <>
+      <h1>Informacion de Pokemon</h1>
+      <hr />
+      {isLoading && <p>Loading...</p>}
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <h2>{data?.name}</h2>
+    </>
+  );
 };
--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = () => {
+export const useFetch = (url) => {
   const [state, setState] = useState({
     data: null,
     isLoading: true,
@@ -10,11 +10,49 @@ export const useFetch = () => {
 
   useEffect(() => {
     getFetch();
-  }, []);
+  }, [url]);
+
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null,
+    });
+  };
 
   const getFetch = async () => {
-    const resp = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    setLoadingState();
+
+    const resp = await fetch(url);
+
+    //Sleep
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    if (!resp.ok) {
+      setState({
+        data: null,
+        isLoading: false,
+        hasError: true,
+        error: {
+          code: resp.status,
+          message: resp.statusText,
+        },
+      });
+      return;
+    }
+
     const data = await resp.json();
+
+    setState({
+      data,
+      isLoading: false,
+      hasError: false,
+      error: null,
+    });
+
+    //Manejo del cache
+
     console.log({ data });
   };
 
