afeab7e772f66996343c48886ba74027cb913201
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-10 23:05:43 -0600

    Ejemplo usando el return del useEffect

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,17 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setcoords] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
-    console.log("Message mounted");
+    const onMouseMove = ({ x, y }) => {
+      //const coords = { x, y };
+      setcoords({ x, y });
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
 
     return () => {
-      console.log("Message unmounted");
+      window.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
   return (
     <>
       <h3>Usuario ya existe</h3>
+      {JSON.stringify(coords)}
     </>
   );
 };
