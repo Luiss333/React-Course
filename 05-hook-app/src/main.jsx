4d01bbf316bdd1950645823706d88f76faaf45f2
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-11 05:47:54 -0600

    FocusScreen: Agregado. Usando el hook: useRef

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,10 +6,11 @@ import "./index.css";
 // import { CounerWithCustomHook } from "./01-useState/CounerWithCustomHook";
 //import { SimpleForm } from "./02-useEffect/SimpleForm";
 //import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+//import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <MultipleCustomHooks />
+  <FocusScreen />
   // </React.StrictMode>
 );
