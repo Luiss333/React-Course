6633f41997f886ae4bb4d393f03678c717b33ebc
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-11 04:32:14 -0600

    LoaddingMessage: agregado. PokemonCard: agregado. Utilizando varios customhooks

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,17 +1,43 @@
 import React from "react";
-import { useFetch } from "../hooks";
+import { useCounter, useFetch } from "../hooks";
+import { LoadingMessage } from "./LoadingMessage";
+import { PokemonCard } from "./PokemonCard";
 
 export const MultipleCustomHooks = () => {
+  const { counter, decrement, increment } = useCounter(1);
   const { data, isLoading, hasError } = useFetch(
-    "https://pokeapi.co/api/v2/pokemon/3"
+    `https://pokeapi.co/api/v2/pokemon/${counter}`
   );
+  console.log(data);
   return (
     <>
       <h1>Informacion de Pokemon</h1>
       <hr />
-      {isLoading && <p>Loading...</p>}
+      {isLoading ? (
+        <LoadingMessage />
+      ) : (
+        <PokemonCard
+          id={data.id}
+          name={data.name}
+          sprites={[
+            data.sprites.front_default,
+            data.sprites.front_shiny,
+            data.sprites.back_default,
+            data.sprites.back_shiny,
+          ]}
+        />
+      )}
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      <h2>{data?.name}</h2>
+      {/* <h2>{data?.name}</h2> */}
+      <button
+        className="btn btn-primary"
+        onClick={() => (counter > 1 ? decrement() : null)}
+      >
+        Anterior
+      </button>
+      <button className="btn btn-primary" onClick={() => increment()}>
+        Siguiente
+      </button>
     </>
   );
 };
