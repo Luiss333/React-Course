959f62b3cdd515c91fc6336891a6f886af5da7d9
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-10 21:24:35 -0600

    useEffect Agregado

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HooksApp } from "./HooksApp";
-import { CounterApp } from "./01-useState/CounterApp";
-import { CounerWithCustomHook } from "./01-useState/CounerWithCustomHook";
 import "./index.css";
+// import { HooksApp } from "./HooksApp";
+// import { CounterApp } from "./01-useState/CounterApp";
+// import { CounerWithCustomHook } from "./01-useState/CounerWithCustomHook";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CounerWithCustomHook />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <SimpleForm />
+  // </React.StrictMode>
 );
