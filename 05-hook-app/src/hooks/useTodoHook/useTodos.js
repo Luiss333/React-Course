512d30d67358136fe9d5df3d3807928c90cf7b83
Luis G Martinez G <luis.martinez0125@hotmail.com>
2024-04-26 18:16:10 -0600

    Algunos comentarios añadidos

--- 05-hook-app/src/hooks/useTodoHook/useTodos.js
+++ 05-hook-app/src/hooks/useTodoHook/useTodos.js
@@ -15,6 +15,13 @@ const init = () => {
 
 export const useTodos = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+  // Usamos useReducer para manejar el estado de los todos en lugar de useState
+  //    para tener un mejor control de las acciones que se realizan sobre el estado,
+  //    donde dispatch es la función que se encarga de enviar las acciones al reducer.
+  // También usamos un inicializador init para obtener los todos del localStorage si existen.
+  // todoReducer es una función que se encarga de manejar las acciones que se realizan
+  //    sobre el estado de los todos.
+  // InitialState es un arreglo de objetos que representan los todos.
 
   const [todosCount, setTodosCount] = useState(0);
   const [pendingTodosCount, setPendingTodosCount] = useState(0);
@@ -36,7 +43,7 @@ export const useTodos = () => {
   const handleDeleteTodo = (id) => {
     dispatch({
       type: "[TODO] Remove Todo",
-      payload: id,
+      payload: id, //Payload funciona como un argumento que se envía al reducer
     });
   };
 
